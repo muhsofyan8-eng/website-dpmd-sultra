c0b11bf904ae93bee4b65a0c1ee24437dc93615e
muhsofyan8-eng <muhsofyan8@gmail.com>
2026-02-14 20:55:29 +0800

    Create profil overview page

---
+++ profil/index.js
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+const ProfileOverview = () => {
+  return (
+    <div>
+      <h1>Profil Overview</h1>
+      <p>Welcome to the profile overview page.</p>
+    </div>
+  );
+};
+
+export default ProfileOverview;
